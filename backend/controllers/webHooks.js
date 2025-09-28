9b49312612cf899e58d418b15e4f779967ebcdc1
Sharang Patel <103412062+sharangpatel@users.noreply.github.com>
2025-09-28 20:59:24 +0530

    Update webHooks.js

--- backend/controllers/webHooks.js
+++ backend/controllers/webHooks.js
@@ -1,9 +1,12 @@
+
+
 import Stripe from "stripe";
 import Transaction from '../models/Transaction.js';
 import User from '../models/User.js'
 
 export const stripeWebHooks = async(request,response)=>{
-    const stripe = new Stripe(process.env.STRIPE_WEBHOOK_SECRET)
+    // Use Stripe Secret Key for API operations
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY); 
     const sig = request.headers["stripe-signature"];
 
     let event;
@@ -14,34 +17,37 @@ export const stripeWebHooks = async(request,response)=>{
         return response.status(400).send(`Webhook error ${error.message}`)
     }
 
+    //This try catch is for checking event
     try {
         switch(event.type){
-            case "payment_intent.succeeded":{
+            case "payment_intent.succeeded": {
                 const paymentIntent = event.data.object;
-                const sessionList = await stripe.checkout.sessions.list({
-                    payment_intent:paymentIntent.id
-                })
-                const session = sessionList.data[0];
-                const {transactionId, appId} = session.metadata;
+                const sessionList = await stripe.checkout.sessions.list({ payment_intent: paymentIntent.id
+                });
 
-                if(appId === 'chatYoda'){
-                    const transaction = await Transaction.findOne({transactionId, isPaid:false})
-                    
-                //update credits in user account
-                await User.updateOne({_id:transaction.userId},{$inc:{credits:transaction.credits}})
-                
-                //update credit payment status
-                transaction.isPaid = true;
-                await transaction.save()
+                const session = sessionList.data[0];
+                const { transactionId, appId } = session.metadata;
+
+                if(appId === 'chatYoda' || !transactionId){
+                    const transaction = await Transaction.findOne({_id: transactionId, isPaid: false});
+
+                    //update credits in user account
+                    await User.updateOne({_id: transaction.userId}, { $inc: { credits: transaction.credits } });
+
+                    //update credit payment status
+                    transaction.isPaid = true;
+                    await transaction.save();
                 }else{
-                    return response.json({received:true, message:'Ignored event, Invalid app'})
-                }
-                break;
+                    return response.json({received:true, message:'Ignored event, invalid app or transactionId'})
+                }break;
+
             }
-        default:console.log('Unhandled event type : ', event.type)
-            break;
+
+            default:
+                console.log('Unhandled event type : ', event.type)
+                break;
         }
-        response.json({received:true})
+        response.json({received:true})        
     } catch (error) {
         console.log("Webhook processing error: ",error)
         response.status(500).send("Internal server Error")
