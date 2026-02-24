b9830137d3bcc21639cccf3fe15dae5326915be9
Sharang Patel <103412062+sharangpatel@users.noreply.github.com>
2026-02-24 18:22:14 +0530

    Fix user ID reference in deleteChat function

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -38,10 +38,10 @@ export const getChats = async(req,res)=>{
 
 export const deleteChat = async(req,res)=>{
     try {
-        const userId = req.user_id;
+        const userId = req.user._id;
         const {chatId} = req.body; 
         
-        await Chat.deleteOne({id:chatId,userId})
+        await Chat.deleteOne({_id:chatId,userId})
 
         res.json({success:true,message:'Chat deleted'})
     } catch (error) {
